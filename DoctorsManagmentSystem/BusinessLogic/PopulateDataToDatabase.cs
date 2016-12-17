8c847a91ffc6a071868689532e8dc7262189235f
YOGESHVEDPATHAK333 <yogeshvedpathak333@gmail.com>
2016-12-17 16:17:02 +0530

    code modified

---
+++ DoctorsManagmentSystem/BusinessLogic/PopulateDataToDatabase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DoctorsManagmentSystem.Interface;
+using DoctorsManagmentSystem.Models;
+
+namespace DoctorsManagmentSystem.BusinessLogic
+{
+    public class PopulateDataToDatabase : IPopulateDataToDatabase
+    {
+        private Models.DoctorsManagementSystemEntities db;
+
+        public PopulateDataToDatabase(DoctorsManagementSystemEntities db)
+        {
+            // TODO: Complete member initialization
+            this.db = db;
+        }
+        public void BatchInsertion(List<Doctor> doctors, string tableTable)
+        {
+            switch (tableTable.ToLower())
+            {
+                case "doctor":
+                    db.Doctors.AddRange(doctors);
+                    break;
+                    
+            }
+            db.SaveChanges();
+        }
+    }
+}
