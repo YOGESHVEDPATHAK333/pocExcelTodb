7e7c28e9ca5f0e95c659e62e7c475eabb2f88ad7
YOGESHVEDPATHAK333 <yogeshvedpathak333@gmail.com>
2016-12-30 19:07:10 +0530

    "Sample Project on Unit Testing"

---
+++ Calculator/Calculator.Tests/CalculatorOpTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+   public class CalculatorOpTest
+    {
+        [TestMethod]
+        public void ShouldAddReurnNineWhenPassFiveandFour()
+        {
+            CalculatorOp sut = new CalculatorOp();
+            int result = sut.Add(5,4);
+            Assert.AreEqual(9,result);
+        }
+        [TestMethod]
+        public void ShouldMulReurnTwentyWhenPassFiveandFour()
+        {
+            CalculatorOp sut = new CalculatorOp();
+            int result = sut.Mul(5, 4);
+            Assert.AreEqual(20, result);
+        }
+    }
+}
---
+++ Calculator/CalculatorNUnitTest/CalculatorOpTest.cs
@@ -0,0 +1,24 @@
+﻿using Calculator;
+using NUnit.Framework;
+
+namespace CalculatorNUnitTest
+{
+    [TestFixture]
+    public class CalculatorOpTest
+    {
+        [Test]
+        public void ShouldAddReurnNineWhenPassFiveandFour()
+        {
+            CalculatorOp sut = new CalculatorOp();
+            int result = sut.Add(5, 4);
+            Assert.That(result,Is.EqualTo (9));
+        }
+        [Test]
+        public void ShouldMulReurnNineWhenPassFiveandFour()
+        {
+            CalculatorOp sut = new CalculatorOp();
+            int result = sut.Add(5, 4);
+            Assert.That(result, Is.EqualTo(20));
+        }
+    }
+}
